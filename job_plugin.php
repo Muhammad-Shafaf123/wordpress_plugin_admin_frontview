1b8862249b54a6a9943b41f8fbdd06dbee903e01
Muhammed Shafaf <shafafmuhammad6@gmail.com>
2021-10-11 20:28:32 +0530

    error not fix, update post meta not working

--- job_plugin.php
+++ job_plugin.php
@@ -13,6 +13,9 @@ class CustomTaxomony{
   public function __construct(){
     add_action('init',array($this,'create_post_type'));
     add_action('add_meta_boxes',array($this, 'meta_box_callback'));
+    add_action('admin_menu',array($this, 'sub_admin_menu'));
+    add_action( 'save_post', array($this,'save_post_data'));
+
   }
   public function create_post_type(){
     register_post_type('job-manage',
@@ -28,16 +31,36 @@ class CustomTaxomony{
   }
   public function form_field_callback(){
     ?>
-    <label for="fname">First name:</label><br>
-    <input type="text" id="fname" name="fname" value="John"><br>
-    <label for="lname">Last name:</label><br>
-    <input type="text" id="lname" name="lname" value="Doe"><br><br>
-    <input type="submit" value="Submit">
+    <form  action="" method="post">
+      <label for="job types">job types</label><br />
+      <input type="checkbox" name="full_time" value="full_time"/>
+      <label for="full_time">full time</label><br />
+      <input type="checkbox" name="part_time" value="part_time"/>
+      <label for="part_time">part time</label><br />
+      <input type="checkbox" name="trainee" value="trainee"/>
+      <label for="job types">trainee</label><br />
+      <input type="checkbox" name="intership" value="intership" />
+      <label for="job types">intership</label><br />
+    </form>
     <?php
   }
-  
+  public function sub_admin_menu() {
+  add_submenu_page('edit.php?post_type=job-manage',
+                    'wpautop-control',
+                    'Settings',
+                    'administrator',
+                    'settings',
+                    array($this,'call_back'));
+  }
+  public function call_back(){
+    echo "hai";
+  }
+  public function save_post_data(){
+    if (isset($_POST['full_time'])) {
+        update_post_meta($post_id,'job_type_meta_key',$_POST['full_time']);
+  }
 }
 
-new CustomTaxomony();
+new  CustomTaxomony;
 
  ?>
