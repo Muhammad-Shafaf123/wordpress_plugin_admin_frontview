6f2d3dfaadd59ca83077944a82c61f6487f4760b
Muhammed Shafaf <shafafmuhammad6@gmail.com>
2021-10-13 16:41:32 +0530

    settings UI done,setting data base connection pending

--- job-settings.php
+++ job-settings.php
@@ -1,44 +1,62 @@
 <?php
 
-class ClassAdminSettings{
-  function __construct(){
-    add_action('admin_menu',array($this, 'sub_admin_menu'));
-    add_action( 'admin_init', array( $this, 'setup_sections' ) );
-    add_action( 'admin_init', array( $this, 'setup_fields' ) );
+class ClassAdminSettings {
+  function __construct() {
+    add_action('admin_menu', array($this, 'sub_admin_menu'));
+    add_action('admin_init', array($this, 'setup_sections'));
+    add_action('admin_init', array($this, 'setup_fields'));
   }
-
+  //create section
   public function setup_sections() {
-    add_settings_section( 'our_first_section', 'My First Section Title',
-                        array( $this, 'section_callback' ), 'smashing_fields' );
-    add_settings_section( 'our_second_section', 'My Second Section Title',
-                        array( $this, 'section_callback' ), 'smashing_fields' );
-    add_settings_section( 'our_third_section', 'My Third Section Title',
-                        array( $this, 'section_callback' ), 'smashing_fields' );
+    add_settings_section('first_section', 'General Settings',
+                        array($this, 'section_callback'), 'jobs');
+
   }
-
-  public function section_callback( $arguments ) {
-    switch( $arguments['id'] ){
-        case 'our_first_section':
-            echo 'This is the first description here!';
-            break;
-        case 'our_second_section':
-            echo 'This one is number two';
-            break;
-        case 'our_third_section':
-            echo 'Third time is the charm!';
-            break;
+  public function section_callback($arguments) {
+    if($arguments['id']){
+      echo "section details.";
     }
-}
+  }
+  public function setup_fields() {
+      add_settings_field('first_field_id', 'Common Link :',
+      array($this, 'common_link_field_callback'), 'jobs', 'first_section');
+      add_settings_field('second_field_id', 'Company Name',
+      array($this, 'text_area_field_callback'), 'jobs', 'first_section');
+      add_settings_field('third_field_id', 'Job Type',
+      array($this, 'job_type_field_callback'), 'jobs', 'first_section');
+      add_settings_field('fourth_field_id', 'Instructions',
+      array($this, 'instruction_area_field_callback'), 'jobs', 'first_section');
+      register_setting('sample', 'first_field_id');
+      register_setting('sample', 'second_field_id');
+      register_setting('sample', 'third_field_id');
+      register_setting('sample', 'fourth_field_id');
 
-public function setup_fields() {
-    add_settings_field( 'our_first_field', 'Field Name',
-      array( $this, 'field_callback' ), 'smashing_fields', 'our_first_section' );
-}
-public function field_callback( $arguments ) {
-    echo '<input name="our_first_field" id="our_first_field"
-          type="text" value="' . get_option( 'our_first_field' ) . '" />';
-    register_setting( 'smashing_fields', 'our_first_field' );
-}
+
+  }
+  public function common_link_field_callback($arguments) {
+      ?>
+      <input name="common_link_name" id="common_link_name"
+             type="text" placeholder="www.companyname.com"  value="<?php get_option('first_field_id') ?>"/>'
+      <?php
+  }
+  public function text_area_field_callback($arguments) {
+      ?>
+      <input name="common_link_name" id="common_link_name"
+             type="text" placeholder="Conpany Name"  value="<?php get_option('second_field_id') ?>"/>'
+      <?php
+  }
+  public function instruction_area_field_callback($arguments) {
+      ?>
+      <textarea id="instructio_text" name="instructio_text" rows="4" cols="50">
+     </textarea>
+      <?php
+  }
+  public function job_type_field_callback($arguments) {
+      ?>
+      <input type="radio" name="job-full-checked" value="<?php get_option('four_field_id') ?>">full time
+      <input type="radio" name="job-part-checked" value="">part time
+      <?php
+  }
   //create submenu.
   function sub_admin_menu() {
   add_submenu_page('edit.php?post_type=job-manage',
@@ -48,16 +66,14 @@ public function field_callback( $arguments ) {
                     'settings',
                     array($this,'call_back'));
   }
-
   //call back function for settings.
-
     public function call_back() { ?>
-      <div class="wrap" action="options.php">
+       <div class="wrap">
           <h2>My Awesome Settings Page</h2>
-          <form method="post">
+          <form method="post" action="options.php">
             <?php
-                settings_fields( 'smashing_fields' );
-                do_settings_sections( 'smashing_fields' );
+                settings_fields('sample');
+                do_settings_sections('jobs');
                 submit_button();
             ?>
           </form>
@@ -65,12 +81,12 @@ public function field_callback( $arguments ) {
   }
 
 
-  function write_log ( $log )  {
-		if ( true === WP_DEBUG ) {
-			if ( is_array( $log ) || is_object( $log ) ) {
-				error_log( print_r( $log, true ) );
-			} else {
-				error_log( $log );
+  function write_log($log) {
+		if(true === WP_DEBUG) {
+			if(is_array($log) || is_object($log)) {
+				error_log(print_r($log, true));
+			}else {
+				error_log($log);
 			}
 		}
 	}
--- job_plugin.php
+++ job_plugin.php
@@ -10,39 +10,36 @@
 
 // connect to pages.
 require_once('job-settings.php');
-require_once('include.php');
-
-
+require_once('job-content.php');
 // root class for creating custom taxomony.
 class RootPluginClass
 {
-  public function __construct(){
-    add_action('init',array($this,'create_post_type'));
-    add_action('add_meta_boxes',array($this, 'meta_box_callback_qualification'));
-    add_action('add_meta_boxes',array($this, 'meta_box_callback_salary'));
+  public function __construct() {
+    add_action('init', array($this,'create_post_type'));
+    add_action('add_meta_boxes', array($this, 'meta_box_callback_qualification'));
+    add_action('add_meta_boxes' ,array($this, 'meta_box_callback_salary'));
   }
   // create custom post type.
-  public function create_post_type()
-  {
+  public function create_post_type() {
     register_post_type('job-manage',
                         array($this, 'labels'=>
-                        array($this,'name'=>__('Job Manager'),'singular_name' =>__('jobs')),
-                        'public' => true,'has_archive' => true, 'rewrite'=>
-                        array($this,'slug'=>'job-manage'),
+                        array($this, 'name'=>__('Job Manager'),
+                        'singular_name' =>__('jobs')),
+                        'public' => true, 'has_archive' => true,
+                        'taxonomies' => array( 'category', 'post_tag' ),
+                        'rewrite'=> array($this, 'slug'=>'job-manage'),
                         )
                       );
   }
   // meta box for qualification
-  public function meta_box_callback_qualification()
-  {
+  public function meta_box_callback_qualification() {
     add_meta_box('meta_1', 'Field', array($this, 'form_field_callback'));
   }
   // meta box for adding salary.
-  public function meta_box_callback_salary()
-  {
+  public function meta_box_callback_salary() {
     add_meta_box('meta_id', 'Drop down', array($this, 'drop_down_meta_callback'));
   }
-  public function drop_down_meta_callback(){
+  public function drop_down_meta_callback() {
     ?>
       <label for="salary_type">Choose salary:</label>
       <select name="salary_types" id="salary_type">
@@ -52,31 +49,25 @@ class RootPluginClass
       <option value="above 40000">above 40000</option>
       <?php wp_nonce_field( 'nonce_action', 'nonce_field' ); ?>
       </select>
-
     <?php
   }
   // function for creating field.
-  public function form_field_callback()
-  {
+  public function form_field_callback() {
     ?>
       <label for="Qualification">Qualification :</label><br />
       <input type="text" name="qualification_select" placeholder="Enter the qualification"/>
       <?php wp_nonce_field( 'nonce_action', 'nonce_field' );
-
-
   }
   // write log function.
-	public function write_log ( $log )  {
+	public function write_log ($log)  {
 		if ( true === WP_DEBUG ) {
-			if ( is_array( $log ) || is_object( $log ) ) {
-				error_log( print_r( $log, true ) );
+			if ( is_array( $log ) || is_object($log)) {
+				error_log(print_r($log, true));
 			} else {
 				error_log( $log );
 			}
 		}
 	}
-
-
 }
 
 new RootPluginClass;
